5a155ac522376a518bf5205177912e22428bfcb8
marco <serratemarco@gmail.com>
2024-09-27 15:56:58 -0300

    Comenzamos de nuev2

--- ClasesyObjetos.py
+++ ClasesyObjetos.py
@@ -1,18 +1,19 @@
 class Celular:
-     #Constructor
+#Metodo Constructor / _init_ es un metodo especial / self es una forma de hacer referencia así mismo
     def __init__(self, marca, modelo, camara):
-      #Atributos
-      self.marca = marca
-      self.modelo = modelo
-      self.camara = camara
+      #Atributos de instancia
+      self.marca = marca;
+      self.modelo = modelo;
+      self.camara = camara;
       #Metodos
     def llamar(self):
         print(f"Estas llamando desde tu: {self.modelo} ")
     def cortar(self):
         print(f"Vas a cortar la llamada desde tu: {self.modelo} ")
-      #Objetos / Intancia de una clase
+      #Creando Objetos / Intancia de una clase
 celular1 = Celular("Sansung", "S23", "48MP")
 celular2 = Celular("Iphone 15", "Iphone 15 ProMax", "500MP" )
+#Imprimiendo objetos
 print("El celular es: " + celular1.marca, celular1.modelo, celular1.camara )
 print("El celular es: "+ celular2.marca, celular2.modelo, celular2.camara )
     #LLamando a un metodo
